016968108bcea45a4c3c633dcc16a0fa651df2e8
kamozin <hrolenkov@yandex.ru>
2018-11-21 10:30:58 +0300

    home commit

--- app/js/script.js
+++ app/js/script.js
@@ -2,3 +2,9 @@ document.addEventListener('DOMContentLoaded', function() {
   svg4everybody();
   console.log('script.js');
 });
+
+$('[data-highlight-news]').slick({
+  speed: 750,
+  arrows: false,
+  dots: true,
+});
